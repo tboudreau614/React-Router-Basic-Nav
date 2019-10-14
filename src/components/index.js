34965741af61de428d4e6e18733d7fb3627628c9
Tristan Boudreau <tristanboudreau@Tristans-MacBook-Pro.local>
2019-10-14 16:41:36 -0600

    mvp

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
